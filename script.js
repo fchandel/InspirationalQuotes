227f1694eafc9b69a18bd4fbb74b5661f8669658
fchandel <fchandel@sfu.ca>
2018-05-18 17:34:29 -0700

    testing if tap to refresh quote works

--- script.js
+++ script.js
@@ -15,33 +15,44 @@ function saveQuotes(quoteObj) {
 	document.getElementById("quoteAuthor").innerHTML = post.title;
 }
 
+//call api by appending the script to our document. Bypasses cross-origin error
 function loadQuote() {
     var s = document.createElement("script");
     s.src = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&_jsonp=saveQuotes";
     document.body.appendChild(s);
 } 
 
+//calls loadQuote() on page load
 document.addEventListener("DOMContentLoaded", loadQuote());
+
+//on hitting spacebar, call refreshQuote()
 document.body.onkeyup = function(e){
     if(e.which == 32){
         refreshQuote();
     }
 }
 
+
+//On load, check if the device is mobile.
+//hide the loading animation
+//and show quote
+
 window.onload = function() { 
-	document.getElementById("loading").style.display = "none";
-	document.getElementById("refreshLabel").style.display = "block";
-	document.getElementById("quote").className = "quote";
 
-} 
-
-document.ready(function() {      
-    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+	var isMobile = window.matchMedia("only screen and (max-width: 760px)");
 
     if (isMobile.matches) {
         //Conditional script here
         window.addEventListener('touchstart', function() {
   			refreshQuote();
+  			document.getElementById("refreshLabel").style.display = "none";
 		});
+    } else {
+		document.getElementById("refreshLabel").style.display = "block";
     }
- });
+
+    
+	document.getElementById("loading").style.display = "none";
+	document.getElementById("quote").className = "quote";
+
+} 
